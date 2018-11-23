171c4ed595606344746bb25a6e78892df873ff9a
Christian Skomorowski <christianskomorowski@outlook.com>
2018-11-22 22:41:16 -0500

    Revert "merge"
    This reverts commit 641693275182de93c18a56d1e30d54e5d62fea91.

---
+++ Game Server/Game Server/ServerGame.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "ServerNetwork.h"
+#include "Packet.h"
+#include <vector>
+#include "ClientPair.h"
+#include "Tokenizer.h"
+#include "Player.h"
+#include "BoxCollider.h"
+
+#include "include/glm/glm.hpp"
+#include "include/glm/ext.hpp"
+#include "Timer.h"
+
+class ServerGame
+{
+
+public:
+
+	ServerGame(void);
+	~ServerGame(void);
+
+	void update();
+	void receiveFromClients();
+
+	std::vector<ClientPair*> pairs;
+
+private:
+
+	// IDs for the clients connecting for table in ServerNetwork 
+	static unsigned int client_id;
+
+	// The ServerNetwork object 
+	ServerNetwork* network;
+
+	// data buffer
+	char network_data[MAX_PACKET_SIZE];
+
+	Player p[2] = { Player(1),Player(2) };
+
+	//collision stuffs
+	std::vector<BoxCollider> collisionBoxes;
+
+	glm::vec3 prevPosition1 = {0.0f,0.0f,0.0f};
+	glm::vec3 prevPosition2 = {0.0f, 0.0f, 0.0f};
+
+	void pairClients(int);
+	void sendMessage(int clientid, int message_type, std::string message);
+	bool collisionCheck(Player);
+
+	void handleIncomingKey(const std::vector<std::string>&);
+	void handleIncomingTransformation(const std::vector<std::string>&);
+	void handleIncomingCollider(const std::vector<std::string>&);
+	void handleIncomingRotation(const std::vector<std::string>&);
+	void handleAttack(const std::vector<std::string>&);
+
+	unsigned int currentClient;
+
+	bool start = false;
+
+	/* Data Members */
+	Timer *updateTimer = nullptr;
+	float TotalGameTime = 0.0f;
+
+
+	void handleJump( int player);
+	void handleAttackBox(int player);
+	void restart();
+};
